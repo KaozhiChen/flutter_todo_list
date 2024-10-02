7aefbce727a35f43f46e51ec2fcc07661672e0aa
Kaozhi Chen <kylerchen16@gmail.com>
2024-10-01 20:15:40 -0400

    Implement simple add task function and display task list

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _inputController = TextEditingController();
+  final List<String> _taskList = [];
+
+  void _addTask() {
+    setState(() {
+      if (_inputController.text.isNotEmpty) {
+        _taskList.add(_inputController.text);
+        _inputController.clear();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +47,36 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Todo List'),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _inputController,
+                    decoration: const InputDecoration(
+                      labelText: 'Please enter a task',
+                    ),
+                  ),
+                ),
+                ElevatedButton(onPressed: _addTask, child: const Text('Add'))
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _taskList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_taskList[index]),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
